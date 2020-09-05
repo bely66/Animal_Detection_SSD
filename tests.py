2666303928c1ca4abd465f3e821c91ece7b628f7
Mohamed Nabil <mohamed.nabil@slickblox.com>
2020-09-05 15:12:12 +0200

    animal data loading

---
+++ tests.py
@@ -0,0 +1,78 @@
+import os
+import json
+
+voc_labels = ('waterdeer', 'wildpig', 'roedeer')
+label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
+label_map['background'] = 0
+rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
+def parse_annotation(annotation_path):
+    with open(annotation_path, "r") as f:
+        annot = f.read().splitlines()
+    objs = annot[1:]
+
+    boxes = list()
+    labels = list()
+    difficulties = list()
+    for object in objs:
+
+        difficult = False
+        data = object.split()
+        label = data[-1]
+        if label not in label_map:
+            continue
+
+        bbox = data[:4]
+        xmin = int(bbox[0]) - 1
+        ymin = int(bbox[1]) - 1
+        xmax = int(bbox[2]) - 1
+        ymax = int(bbox[3]) - 1
+
+        boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(label_map[label])
+        difficulties.append(difficult)
+
+    return {'boxes': boxes, 'labels': labels, 'difficulties': difficulties}
+
+def create_data_lists(animal_path, output_folder):
+    """
+    Create lists of images, the bounding boxes and labels of the objects in these images, and save these to file.
+
+    :param animal_path: path to the 'Images' folder
+    :param output_folder: folder where the JSONs must be saved
+    """
+    animal_path = os.path.abspath(animal_path)
+    animal_files = os.listdir(animal_path)
+    img_path = "./Boxing_KNPS_image/Images/001"
+    print(animal_files)
+    train_images = list()
+    train_objects = list()
+    n_objects = 0
+
+    # Training data
+    for path in animal_files:
+        txt_file = os.path.join(animal_path, path)
+        # Find IDs of images in training data
+        
+        file_name = path.split(".txt")[0]
+        
+        objects = parse_annotation(txt_file)
+        if len(objects['boxes']) == 0:
+            continue
+        n_objects += len(objects)
+        train_objects.append(objects)
+        train_images.append(os.path.join(img_path, file_name + '.jpg'))
+
+    assert len(train_objects) == len(train_images)
+
+    # Save to file
+    with open(os.path.join(output_folder, 'TRAIN_images.json'), 'w') as j:
+        json.dump(train_images, j)
+    with open(os.path.join(output_folder, 'TRAIN_objects.json'), 'w') as j:
+        json.dump(train_objects, j)
+    with open(os.path.join(output_folder, 'label_map.json'), 'w') as j:
+        json.dump(label_map, j)  # save label map too
+
+    print('\nThere are %d training images containing a total of %d objects. Files have been saved to %s.' % (
+        len(train_images), n_objects, os.path.abspath(output_folder)))
+
+create_data_lists("Boxing_KNPS_image/Labels/","./")
